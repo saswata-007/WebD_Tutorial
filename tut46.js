624969a5d26796e38a943b63d1bed2c5b55fd3c6
Saswata-007 <saswatakaya@gmail.com>
2021-07-11 19:09:27 +0530

    init

---
+++ tut46.js
@@ -0,0 +1,15 @@
+
+var str = "This is a string";
+console.log(str);
+
+//First occurance of a substring
+var position = str.indexOf('is');
+console.log(position);
+
+//Last occurance of a substring
+var position1 = str.lastIndexOf('is');
+console.log(position1);
+
+//Substring from a string
+var substr = str.slice(1,6);
+console.log(substr);

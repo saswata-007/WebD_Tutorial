0725ecd0d9af389aa29d79e41e064be24cf8ff84
Saswata-007 <saswatakaya@gmail.com>
2021-07-20 20:22:53 +0530

    init

---
+++ tut57.js
@@ -0,0 +1,32 @@
+// Math Object in JavaScript
+
+console.log("Math Object in JavaScript");
+
+// Printing the math object
+let m  = Math;
+console.log(m); //To see all the math functions available
+
+// Printing the constraints in math object
+console.log("The value of Math.E is", Math.E);
+console.log("The value of Math.LN2 is", Math.LN2);
+console.log("The value of Math.LN10 is", Math.LN10);
+console.log("The value of Math.LOG2E is", Math.LOG2E);
+console.log("The value of Math.PI is", Math.PI);
+console.log("The value of Math.SQRT1_2 is", Math.SQRT1_2);
+console.log("The value of Math.SQRT2 is", Math.SQRT2);
+
+// Printing the functions in math object
+let a = 25.54;
+let b = 40;
+console.log(`The value of a and b is ${a} and ${b}`);
+console.log(`The value of a and b rounded is ${Math.round(a)} and ${Math.round(b)}`);
+
+console.log("3 to the powers 2 is", Math.pow(3,2));
+console.log("2 to the powers 10 is", Math.pow(2,10));
+
+console.log("Using sqrt function", Math.sqrt(100));
+console.log("Using sqrt function", Math.sqrt(37));
+
+// Ceil and Floor
+console.log("5.8 rounded up to nearest integer", Math.ceil(5.8));
+console.log("5.8 rounded down to nearest integer", Math.floor(5.8));

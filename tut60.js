b940d0a7e7e05a9475099361774a32473594114a
Saswata-007 <saswatakaya@gmail.com>
2021-07-24 19:27:27 +0530

    init

--- tut60.js
+++ tut60.js
@@ -1,2 +1,12 @@
 // Node.Js Modules with Examples
 
+const fs = require("fs");
+let text = fs.readFileSync("demo.txt", "utf-8");
+console.log("The content of file is:");
+console.log(text);
+
+text = text.replace("My name is", "I am");
+console.log(text)
+
+console.log("Creating a new file...");
+fs.writeFileSync("demo2.txt",text);

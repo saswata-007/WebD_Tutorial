b1b242a3c55e6e1804745601f65a508ebffa636f
Saswata-007 <saswatakaya@gmail.com>
2021-08-05 18:15:30 +0530

    init

--- tut61.js
+++ tut61.js
@@ -1,1 +1,15 @@
 // Blocking vs Non-Blocking execution
+
+// Syncchronus or blocking
+// -line by line execution
+
+// Asynchronus vs non-blocking
+// -line by line not guranteed
+// -callbacks will fire
+
+const fs = require("fs");
+let text = fs.readFileSync("demo.txt", "utf-8", (err,data)=> {
+    console.log(error,data);
+});    
+console.log("this is a message");
+

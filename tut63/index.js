1778e55a1166843b9cb7ccdb8d85f925942d851a
Saswata-007 <saswatakaya@gmail.com>
2021-08-08 19:18:11 +0530

    init

--- tut63/index.js
+++ tut63/index.js
@@ -1,6 +1,6 @@
+
 const http = require('http');
 const fs = require('fs');
-const { url } = require('inspector');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,25 +11,25 @@ const contact = fs.readFileSync('./contact.html')
 
 const server = http.createServer((req, res)=>{
     console.log(req.url);
-    url = req.url;                                                                                                      
+    url = req.url;
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    if(url=='/home'){
+    if(url == '/'){
         res.end(home);
     }
-    else if(url=='/about'){
+    else if(url == '/about'){
         res.end(about);
     }
-    else if(url=='/services'){
+    else if(url == '/services'){
         res.end(services);
     }
-    else if(url=='/contact'){
+    else if(url == '/contact'){
         res.end(contact);
     }
     else{
         res.statusCode = 404;
-        res.end("<h1>Error: 404; Page not found</h1>");
+        res.end("<h1>404 not found</h1>");
     }
 });
 

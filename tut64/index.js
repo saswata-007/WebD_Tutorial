b125603f4edce80d337235f0544adb6518f76118
Saswata-007 <saswatakaya@gmail.com>
2021-08-11 11:21:38 +0530

    init

---
+++ tut64/index.js
@@ -0,0 +1,8 @@
+const mod = require("./mod");
+// const average = require("./mod");
+// console.log(mod);
+// console.log(average([1,2,3]));
+console.log(mod.avg([1,2,3]));
+console.log(mod.name);
+console.log(mod.repo);
+console.log("This is index.js");

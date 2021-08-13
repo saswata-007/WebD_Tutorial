a9a825f694ad67e52c554d9e859b894f59b0a32e
Saswata-007 <saswatakaya@gmail.com>
2021-08-13 20:19:19 +0530

    init

---
+++ tut65/tut65.js
@@ -0,0 +1,14 @@
+// npm Node Package Manager
+console.log("This is tutorial 65 - npm Node Package Manager")
+
+// /*
+// Software Versoning Notes---
+
+// suppose i create a application so now the first version would be 1.0.0
+// if i fix a bug in the software then it would be upgraded to 1.0.1
+// if i deploy a minor change in the software the it'd be 1.1.0
+// and if i deploy a major change in the software then it'd be 2.0.0   
+
+// summerization --- 
+// (3.2.1 - major.minor.patch // 3 = major change, 2 = minor change, 1 = patch upgrade/bug fix)
+// */

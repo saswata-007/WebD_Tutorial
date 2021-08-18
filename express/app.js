601d18fc6a01a89904a6f47ec2d30fea30e03dbd
Saswata-007 <saswatakaya@gmail.com>
2021-08-18 20:21:05 +0530

    iniit

--- express/app.js
+++ express/app.js
@@ -3,38 +3,45 @@ const path = require("path");
 const app = express();
 const port = 80;
 
-// for serving static files
-app.use('/static', express.static('static'));
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // for serving static files
 
-// Set the template engine as pug
-app.set('view engine', 'pug');
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug'); // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')); // Set the views dirctory
 
-// Set the views dirctory
-app.set('views', path.join(__dirname, 'views'));
-
-// Our pug demo endpoint
-app.get("/demo",(req, res)=>{
-    res.status(200).render('demo',{title:'hey saswata', message: 'hello there, how are you'});
-});
-app.get("/",(req, res)=>{
-    res.status(200).send("This is my homepage of first express app.");
+// ENDPOINT
+app.get('/',(req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely."
+    const params = {'title': 'PES is the best game', 'content': con}
+    res.status(200).render('index.pug', params)
 });
 
-app.get("/about",(req, res)=>{
-    res.send("This is my about page with get request of first express app.");
-});
+// Our pug demo endpoint - CWH VIDEO/TUT - 72 MATERIALS
+// app.get("/demo",(req, res)=>{
+//     res.status(200).render('demo',{title:'hey saswata', message: 'hello there, how are you'});
+// });
+// app.get("/",(req, res)=>{
+//     res.status(200).send("This is my homepage of first express app.");
+// });
 
-app.post("/about",(req, res)=>{
-    res.send("This is my about page wuth post request of first express app.");
-});
+// app.get("/about",(req, res)=>{
+//     res.send("This is my about page with get request of first express app.");
+// });
 
-app.get("/services",(req, res)=>{
-    res.send("This is my services page of first express app.");
-});
+// app.post("/about",(req, res)=>{
+//     res.send("This is my about page wuth post request of first express app.");
+// });
 
-app.get("/contact",(req, res)=>{
-    res.status(404).send("Error Code 404: Page Not Found");
-});
+// app.get("/services",(req, res)=>{
+//     res.send("This is my services page of first express app.");
+// });
+
+// app.get("/contact",(req, res)=>{
+//     res.status(404).send("Error Code 404: Page Not Found");
+// });
+
+
 
 app.listen(port,()=>{
     console.log(`The application started successfully on port ${port}`);

4b33441ae05b687595bda814003e14f21cc38bb4
Saswata-007 <saswatakaya@gmail.com>
2021-08-23 16:05:11 +0530

    init

--- Dance_Website/app.js
+++ Dance_Website/app.js
@@ -9,7 +9,7 @@ app.use('/static', express.static('static')); // for serving static files
 app.use(express.urlencoded())
 
 // PUG SPECIFIC STUFF
-app.set('view engine', 'pug'); // Set    the template engine as pug
+app.set('view engine', 'pug'); // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')); // Set the views dirctory
 
 // ENDPOINT
